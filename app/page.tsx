a79219719edbc92be641c865ef155ac56120e1bf
junhyeung-kim <junhyeung.kim@nuvilab.com>
2024-08-08 14:24:43 +0900

    zz

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
+  const params = useSearchParams();
+
   useEffect(() => {
-    const authUrl = `https://medicals709.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=2QWZnmrfYiZSL70c9jfMzL&state=app_install&redirect_uri=https://cors-test-opal.vercel.app/auth&scope=mall.read_product,mall.write_product`;
+    const mallId = params.get("mall_id");
+
+    if (!mallId) {
+      return;
+    }
+
+    const authUrl = `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=2QWZnmrfYiZSL70c9jfMzL&state=app_install&redirect_uri=https://cors-test-opal.vercel.app/auth&scope=mall.read_product,mall.write_product`;
 
     window.location.href = authUrl;
-  }, []);
+  }, [params]);
 
   return (
     <main>
