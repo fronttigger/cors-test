a11b982ca203aa254d562feb2f0313f3ef16773d
junhyeung-kim <junhyeung.kim@nuvilab.com>
2024-08-08 13:35:25 +0900

    zz

--- app/lib/cafe24Api.ts
+++ app/lib/cafe24Api.ts
@@ -27,7 +27,7 @@ export async function addScriptTag(accessToken: string, mallId: string) {
         display_location: ["all"],
         // skin_code: [3, 4],
         integrity:
-          "sha384-+Z7ELJ6WC7UBf56WsfTWBX52KUh0vvqdXhSUCuu+6hXlwE8QygcMCVARPvTCiFCz",
+          "sha384-LenAMWRJufmjmcvzxQVpaKY01J6tFKejnQVKQBkksNzvAZodIt7MFZI32RUHSkoS",
       },
       {
         headers: {
