90ba1afecc89dd7f3dd5c9df8f1f9710dffc5eae
junhyeung-kim <junhyeung.kim@nuvilab.com>
2024-08-09 13:48:33 +0900

    z

--- app/api/scripttags/route.ts
+++ app/api/scripttags/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: NextRequest) {
       }
     );
 
-    return NextResponse.json(response);
+    return NextResponse.json(response.data);
   } catch (error) {
     console.error("Error to add ScriptTag:", error);
 
