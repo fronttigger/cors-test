0697f0b94b5cfa7c63c15285eb5c948532ae53ee
junhyeung-kim <junhyeung.kim@nuvilab.com>
2024-08-21 10:37:11 +0900

    z

--- public/sample-script.js
+++ public/sample-script.js
@@ -1,81 +1,115 @@
-;(function () {
-  async function updateAPI(cateNo, period) {
-    var apiUrl = `https://cors-test-opal.vercel.app/api/categories/${cateNo}`
+(function () {
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        "https://cors-test-opal.vercel.app/api/scripttags",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-    const response = await fetch(apiUrl, {
-      method: 'PUT',
-      body: JSON.stringify({
-        shop_no: 1,
-        request: {
-          product_display_period: period,
-        },
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+      if (!response.ok) {
+        console.log(`HTTP error! status: ${response.status}`);
+      }
 
-    if (response.ok) {
-      console.log('API 호출 성공:', response)
-      window.location.href =
-        window.origin +
-        window.pathname +
-        `?cate_no=${cateNo}&sort_day=${period}#Product_ListMenu`
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("API 호출 중 오류 발생:", error);
     }
   }
 
-  var urlParams = new URLSearchParams(window.location.search)
-  var cateNo = urlParams.get('cate_no')
-  var sortDay = urlParams.get('sort_day')
-
-  var periodMap = {
-    '1D': '1D',
-    W: 'W',
-    '7D': '7D',
-    '1M': '1M',
+  if (window.location.pathname.includes("/")) {
+    const button = document.createElement("button");
+    button.textContent = "커스텀 기능";
+    button.onclick = async function () {
+      const result = await fetchData();
+      console.log("result", result);
+    };
+    document.body.appendChild(button);
   }
 
-  var newOptions = [
-    { text: '실시간', value: 'W' },
-    { text: '일간', value: '1D' },
-    { text: '주간', value: '7D' },
-    { text: '월간', value: '1M' },
-  ]
+  async function updateAPI(cateNo, period) {
+    const response = await fetch(
+      "https://cors-test-opal.vercel.app/api/categories/" + cateNo,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          shop_no: 1,
+          request: {
+            product_display_period: period,
+          },
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  $('#selArray').empty()
-  $('#selArray').append('<option value="">-정렬방식-</option>')
+    if (response.ok) {
+      console.log("API 호출 성공:", response);
+      window.location.href =
+        window.origin +
+        window.pathname +
+        `?cate_no=${cateNo}&sort_day=${period}#Product_ListMenu`;
+    }
+  }
+
+  var urlParams = new URLSearchParams(window.location.search);
+  var cateNo = urlParams.get("cate_no");
+  var sortDay = urlParams.get("sort_day");
+
+  var periodMap = {
+    "1D": "1D",
+    W: "W",
+    "7D": "7D",
+    "1M": "1M",
+  };
+
+  var newOptions = [
+    { text: "실시간", value: "W" },
+    { text: "일간", value: "1D" },
+    { text: "주간", value: "7D" },
+    { text: "월간", value: "1M" },
+  ];
+
+  $("#selArray").empty();
+  $("#selArray").append('<option value="">-정렬방식-</option>');
 
   $.each(newOptions, function (_, option) {
-    $('#selArray').append(
+    $("#selArray").append(
       '<option data-sort="' +
         option.value +
         '"value="?cate_no=' +
         cateNo +
-        '&sort_day=' +
+        "&sort_day=" +
         option.value +
         '#Product_ListMenu">' +
         option.text +
-        '</option>'
-    )
-  })
+        "</option>"
+    );
+  });
 
   if (sortDay && periodMap[sortDay]) {
-    $('#selArray').val(
-      '?cate_no=' + cateNo + '&sort_day=' + sortDay + '#Product_ListMenu'
-    )
+    $("#selArray").val(
+      "?cate_no=" + cateNo + "&sort_day=" + sortDay + "#Product_ListMenu"
+    );
 
-    updateAPI(cateNo, sortDay)
+    updateAPI(cateNo, sortDay);
   }
 
-  $('#selArray').on('change', function () {
-    var selectedValue = $(this).find('option:selected')
-    var period = selectedValue.data('sort')
+  $("#selArray").on("change", function () {
+    var selectedValue = $(this).find("option:selected");
+    var period = selectedValue.data("sort");
 
     if (cateNo) {
-      updateAPI(cateNo, period)
+      updateAPI(cateNo, period);
     }
-  })
-})()
+  });
+})();
 // ;(function (CAFE24API) {
 //   CAFE24API.get('/api/v2/products/3', function (err, res) {
 //     if (err) {
