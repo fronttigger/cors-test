abb7e9838dc9b64b3b0f6ac73d6b96979bf5aba9
junhyeung-kim <junhyeung.kim@nuvilab.com>
2024-08-21 10:59:15 +0900

    z

--- app/api/scripttags/route.ts
+++ app/api/scripttags/route.ts
@@ -1,157 +1,165 @@
-import { NextRequest, NextResponse } from 'next/server'
-import axios from 'axios'
-import { adminClient } from '@/app/lib/cafe24Api'
-import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import { adminClient } from "@/app/lib/cafe24Api";
+import { cookies } from "next/headers";
 
 const cookieOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: 'none' as const,
-  path: '/',
-}
+  sameSite: "none" as const,
+  path: "/",
+};
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://medicals709.cafe24.com',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers':
-    'Content-Type, Authorization, X-Cafe24-Api-Version',
-  'Access-Control-Allow-Credentials': 'true',
-}
+  "Access-Control-Allow-Origin": "https://medicals709.cafe24.com",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers":
+    "Content-Type, Authorization, X-Cafe24-Api-Version",
+  "Access-Control-Allow-Credentials": "true",
+};
 
 export async function POST(req: NextRequest) {
   try {
-    const data = await req.json()
-    const cookieStore = cookies()
-    let accessToken = cookieStore.get('access_token')?.value
-    const refreshToken = cookieStore.get('refresh_token')?.value
-    const contentType = req.headers.get('Content-Type')
-    const apiVersion = req.headers.get('X-Cafe24-Api-Version')
+    const data = await req.json();
+    const cookieStore = cookies();
+    let accessToken = cookieStore.get("access_token")?.value;
+    const refreshToken = cookieStore.get("refresh_token")?.value;
+    const contentType = req.headers.get("Content-Type");
+    const apiVersion = req.headers.get("X-Cafe24-Api-Version");
 
     if (!accessToken && refreshToken) {
       try {
         const tokenResponse = await adminClient.getAccessTokenUsingRefreshToken(
           {
             refresh_token: refreshToken,
-            client_id: '2QWZnmrfYiZSL70c9jfMzL',
-            client_secret: '6ESfbSGfGkhh2fmkx34NkS',
+            client_id: "2QWZnmrfYiZSL70c9jfMzL",
+            client_secret: "6ESfbSGfGkhh2fmkx34NkS",
           }
-        )
+        );
 
-        console.log('tokenResponse', tokenResponse)
+        console.log("tokenResponse", tokenResponse);
 
-        const { access_token, refresh_token } = tokenResponse.data
+        const { access_token, refresh_token } = tokenResponse.data;
 
-        accessToken = access_token
-        adminClient.setAccessToken(access_token)
+        accessToken = access_token;
+        adminClient.setAccessToken(access_token);
 
-        cookieStore.set('access_token', access_token, {
+        cookieStore.set("access_token", access_token, {
           ...cookieOptions,
           maxAge: 6600, // 1시간 50분 (6600초) 동안 유효
-        })
-        cookieStore.set('refresh_token', refresh_token, {
+        });
+        cookieStore.set("refresh_token", refresh_token, {
           ...cookieOptions,
           maxAge: 14 * 24 * 60 * 60, // 2주 동안 유효
-        })
+        });
       } catch (error) {
-        console.error('Failed to refresh access token:', error)
-        return NextResponse.redirect('/login')
+        console.error("Failed to refresh access token:", error);
+        return NextResponse.redirect("/login");
       }
     }
 
     const response = await axios.post(
-      'https://medicals709.cafe24api.com/api/v2/admin/scripttags',
+      "https://medicals709.cafe24api.com/api/v2/admin/scripttags",
       data,
       {
         headers: {
-          'Content-Type': contentType,
+          "Content-Type": contentType,
           Authorization: `Bearer ${accessToken}`,
-          'X-Cafe24-Api-Version': apiVersion,
+          "X-Cafe24-Api-Version": apiVersion,
         },
         withCredentials: true,
       }
-    )
+    );
 
-    return NextResponse.json(response.data)
+    return NextResponse.json(response.data);
   } catch (error) {
-    console.error('Error to add ScriptTag:', error)
-    return NextResponse.json({ error }, { status: 500 })
+    console.error("Error to add ScriptTag:", error);
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
 
 export async function GET(req: NextRequest) {
+  const cookieStore = cookies();
+
+  let accessTokenTest = cookieStore.get("access_token")?.value;
+  const authorization = req.headers.get("Authorization");
+
+  console.log("test", accessTokenTest);
+  console.log("test2", authorization);
+
   try {
-    const cookieStore = cookies()
-    let accessToken = cookieStore.get('access_token')?.value
-    const refreshToken = cookieStore.get('refresh_token')?.value
-    const contentType = req.headers.get('Content-Type')
-    const apiVersion = req.headers.get('X-Cafe24-Api-Version')
+    const cookieStore = cookies();
+    let accessToken = cookieStore.get("access_token")?.value;
+    const refreshToken = cookieStore.get("refresh_token")?.value;
+    const contentType = req.headers.get("Content-Type");
+    const apiVersion = req.headers.get("X-Cafe24-Api-Version");
 
     if (!accessToken && refreshToken) {
       try {
         const tokenResponse = await adminClient.getAccessTokenUsingRefreshToken(
           {
             refresh_token: refreshToken,
-            client_id: '2QWZnmrfYiZSL70c9jfMzL',
-            client_secret: '6ESfbSGfGkhh2fmkx34NkS',
+            client_id: "2QWZnmrfYiZSL70c9jfMzL",
+            client_secret: "6ESfbSGfGkhh2fmkx34NkS",
           }
-        )
+        );
 
-        console.log('tokenResponse', tokenResponse)
+        console.log("tokenResponse", tokenResponse);
 
-        const { access_token, refresh_token } = tokenResponse.data
+        const { access_token, refresh_token } = tokenResponse.data;
 
-        accessToken = access_token
-        adminClient.setAccessToken(access_token)
+        accessToken = access_token;
+        adminClient.setAccessToken(access_token);
 
-        cookieStore.set('access_token', access_token, {
+        cookieStore.set("access_token", access_token, {
           ...cookieOptions,
           maxAge: 6600, // 1시간 50분 (6600초) 동안 유효
-        })
-        cookieStore.set('refresh_token', refresh_token, {
+        });
+        cookieStore.set("refresh_token", refresh_token, {
           ...cookieOptions,
           maxAge: 14 * 24 * 60 * 60, // 2주 동안 유효
-        })
+        });
       } catch (error) {
-        console.error('Failed to refresh access token:', error)
-        return NextResponse.redirect('/login')
+        console.error("Failed to refresh access token:", error);
+        return NextResponse.redirect("/login");
       }
     }
 
     const response = await axios.get(
-      'https://medicals709.cafe24api.com/api/v2/admin/scripttags/count',
+      "https://medicals709.cafe24api.com/api/v2/admin/scripttags/count",
       {
         headers: {
-          'Content-Type': contentType,
+          "Content-Type": contentType,
           Authorization: `Bearer ${accessToken}`,
-          'X-Cafe24-Api-Version': apiVersion,
+          "X-Cafe24-Api-Version": apiVersion,
         },
         withCredentials: true,
       }
-    )
+    );
 
     const nextResponse = NextResponse.json(response.data, {
       status: 200,
-    })
+    });
 
     Object.entries(corsHeaders).forEach(([key, value]) => {
-      nextResponse.headers.set(key, value)
-    })
+      nextResponse.headers.set(key, value);
+    });
 
-    return nextResponse
+    return nextResponse;
   } catch (error) {
-    console.error('Error to add ScriptTag:', error)
-    return NextResponse.json({ error }, { status: 500 })
+    console.error("Error to add ScriptTag:", error);
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
 
 export async function OPTIONS() {
   const response = new NextResponse(null, {
     status: 204,
-  })
+  });
 
   Object.entries(corsHeaders).forEach(([key, value]) => {
-    response.headers.set(key, value)
-  })
+    response.headers.set(key, value);
+  });
 
-  return response
+  return response;
 }
--- public/sample-script.js
+++ public/sample-script.js
@@ -32,83 +32,83 @@
     document.body.appendChild(button);
   }
 
-  async function updateAPI(cateNo, period) {
-    const response = await fetch(
-      "https://cors-test-opal.vercel.app/api/categories/" + cateNo,
-      {
-        method: "PUT",
-        body: JSON.stringify({
-          shop_no: 1,
-          request: {
-            product_display_period: period,
-          },
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+  // async function updateAPI(cateNo, period) {
+  //   const response = await fetch(
+  //     "https://cors-test-opal.vercel.app/api/categories/" + cateNo,
+  //     {
+  //       method: "PUT",
+  //       body: JSON.stringify({
+  //         shop_no: 1,
+  //         request: {
+  //           product_display_period: period,
+  //         },
+  //       }),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
 
-    if (response.ok) {
-      console.log("API 호출 성공:", response);
-      window.location.href =
-        window.origin +
-        window.pathname +
-        `?cate_no=${cateNo}&sort_day=${period}#Product_ListMenu`;
-    }
-  }
+  //   if (response.ok) {
+  //     console.log("API 호출 성공:", response);
+  //     window.location.href =
+  //       window.origin +
+  //       window.pathname +
+  //       `?cate_no=${cateNo}&sort_day=${period}#Product_ListMenu`;
+  //   }
+  // }
 
-  var urlParams = new URLSearchParams(window.location.search);
-  var cateNo = urlParams.get("cate_no");
-  var sortDay = urlParams.get("sort_day");
+  // var urlParams = new URLSearchParams(window.location.search);
+  // var cateNo = urlParams.get("cate_no");
+  // var sortDay = urlParams.get("sort_day");
 
-  var periodMap = {
-    "1D": "1D",
-    W: "W",
-    "7D": "7D",
-    "1M": "1M",
-  };
+  // var periodMap = {
+  //   "1D": "1D",
+  //   W: "W",
+  //   "7D": "7D",
+  //   "1M": "1M",
+  // };
 
-  var newOptions = [
-    { text: "실시간", value: "W" },
-    { text: "일간", value: "1D" },
-    { text: "주간", value: "7D" },
-    { text: "월간", value: "1M" },
-  ];
+  // var newOptions = [
+  //   { text: "실시간", value: "W" },
+  //   { text: "일간", value: "1D" },
+  //   { text: "주간", value: "7D" },
+  //   { text: "월간", value: "1M" },
+  // ];
 
-  $("#selArray").empty();
-  $("#selArray").append('<option value="">-정렬방식-</option>');
+  // $("#selArray").empty();
+  // $("#selArray").append('<option value="">-정렬방식-</option>');
 
-  $.each(newOptions, function (_, option) {
-    $("#selArray").append(
-      '<option data-sort="' +
-        option.value +
-        '"value="?cate_no=' +
-        cateNo +
-        "&sort_day=" +
-        option.value +
-        '#Product_ListMenu">' +
-        option.text +
-        "</option>"
-    );
-  });
+  // $.each(newOptions, function (_, option) {
+  //   $("#selArray").append(
+  //     '<option data-sort="' +
+  //       option.value +
+  //       '"value="?cate_no=' +
+  //       cateNo +
+  //       "&sort_day=" +
+  //       option.value +
+  //       '#Product_ListMenu">' +
+  //       option.text +
+  //       "</option>"
+  //   );
+  // });
 
-  if (sortDay && periodMap[sortDay]) {
-    $("#selArray").val(
-      "?cate_no=" + cateNo + "&sort_day=" + sortDay + "#Product_ListMenu"
-    );
+  // if (sortDay && periodMap[sortDay]) {
+  //   $("#selArray").val(
+  //     "?cate_no=" + cateNo + "&sort_day=" + sortDay + "#Product_ListMenu"
+  //   );
 
-    updateAPI(cateNo, sortDay);
-  }
+  //   updateAPI(cateNo, sortDay);
+  // }
 
-  $("#selArray").on("change", function () {
-    var selectedValue = $(this).find("option:selected");
-    var period = selectedValue.data("sort");
+  // $("#selArray").on("change", function () {
+  //   var selectedValue = $(this).find("option:selected");
+  //   var period = selectedValue.data("sort");
 
-    if (cateNo) {
-      updateAPI(cateNo, period);
-    }
-  });
+  //   if (cateNo) {
+  //     updateAPI(cateNo, period);
+  //   }
+  // });
 })();
 // ;(function (CAFE24API) {
 //   CAFE24API.get('/api/v2/products/3', function (err, res) {
